94751d482bc3577b8d25b9fe31775518d2b2da5f
NabilahTm <37111828+NabilahTm@users.noreply.github.com>
2018-03-06 22:27:40 +0700

    tugas html5

---
+++ tugasProweb1/index.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Tugas Pemrograma Web HTML</title> 
+        <link rel="icon" href="website.png"
+         
+     
+    </head>
+    <body>
+        <h1><marquee style="font-family: verdana">WELCOME</marquee></h1>
+       
+    <body style="background: pink">
+        <h2 style="color: firebrick "><big>My Profile</big></h2>
+        <h3>1. Data Pribadi</h3>
+        <pre>
+            Nama: Nabilah Tanjach Muna
+            Nim : 5150411109
+            Kls : Informatika B
+        </pre>
+        <h3  style="color:  black">2. Riwayat Pendidikan</h3>
+         <table style="border: 3px black solid">
+        <tr style=" text-align: center">
+            <td style="border: 2px black solid;color: black"> No </td>
+            <td style="border: 2px black solid;color: black"> Nama Sekolah</td>
+            <td style="border: 2px black solid;color: black"> Jurusan</td>
+            <td style="border: 2px black solid;color: black"> Tahun Lulus</td>
+        </tr>
+        
+         <tr>
+            <td style="border: 2px black solid; color:  darkred"> 1.</td>
+            <td style="border: 2px black solid; color: darkred"> SDN 1 Jolotundo</td>
+            <td style="border: 2px black solid; color: darkred;text-align: center"> -</td>
+            <td style="border: 2px black solid; color:  darkred">2009</td>
+        </tr>
+         <tr>
+            <td style="border: 2px black solid; color:  darkred"> 2.</td>
+            <td style="border: 2px black  solid; color:  darkred"> SMPN 1 Lasem</td>
+            <td style="border: 2px black solid; color:  darkred; text-align:  center"> -</td>
+            <td style="border: 2px black solid; color:  darkred">2012</td>
+        </tr>
+         <tr>
+            <td style="border: 2px  solid; color: darkred"> 3.</td>
+            <td style="border: 2px black solid; color:  darkred"> SMAN 1 Lasem</td>
+            <td style="border: 2px black solid; color:  darkred"> IPA</td>
+            <td style="border: 2px black solid; color: darkred">2015</td>
+        </tr>
+         </table>
+        <h3>3. Situs yang saya miliki,silahkan klik dan kunjungi!!</h3>
+        <blockquote>
+            <li><a href="https://nabilahtanjachmuna.blogspot.co.id/">Blog</a></li>
+            <li><a href="https://www.instagram.com/nabillah_tm/">Instagram</a></li>
+            <li><a href="https://web.facebook.com/?_rdc=1&_rdr">Facebook</a></li>
+        </blockquote>
+        <hr>
+       
+    
+        <h3 style="color: purple">Jadwal Kuliah Ku!!!</h3>
+    <table style="border: 3px red solid">
+        <tr style=" text-align: center">
+            <td style="border: 2px black solid;color: red"> Hari </td>
+            <td style="border: 2px black solid;color: red"> Mata Kuliah</td>
+            <td style="border: 2px black solid;color: red"> Ruang</td>
+            <td style="border: 2px black solid;color: red"> Jam</td>
+        </tr>
+        
+        <tr>
+            <td style="border: 2px blue solid" rowspan="2"> Senin</td>
+            <td style="border: 2px blue solid"> Pemrograman Web Praktek</td>
+            <td style="border: 2px blue solid"> LK 07</td>
+            <td style="border: 2px blue solid"> 07.00-10.20</td>
+        </tr>
+         
+        <tr>
+            <td style="border: 2px blue solid"> Administrasi Web Server</td>
+            <td style="border: 2px blue solid"> D01</td>
+            <td style="border: 2px blue solid"> 12.50-15.20</td>
+        </tr>
+        
+         <tr>
+            <td style="border: 2px darkgoldenrod solid"> Rabu</td>
+            <td style="border: 2px darkgoldenrod solid"> Pemrograman Web</td>
+            <td style="border: 2px darkgoldenrod solid"> F01</td>
+            <td style="border: 2px darkgoldenrod solid"> 14.40-16.20</td>
+        </tr>
+         <tr>
+            <td style="border: 2px darkred solid" rowspan="2"> Kamis</td>
+            <td style="border: 2px darkred solid"> Rekayasa Web</td>
+            <td style="border: 2px darkred solid"> D03</td>
+            <td style="border: 2px darkred solid"> 09.40-12.10</td>
+        </tr>
+        <tr>
+            <td style="border: 2px darkred solid"> Pemrograman Client Server</td>
+            <td style="border: 2px darkred solid"> D33</td>
+            <td style="border: 2px darkred solid"> 14.40-16.20</td>
+        </tr>
+        <tr>
+            <td style="border: 2px yellowgreen solid"> Jumat</td>
+            <td style="border: 2px yellowgreen solid"> Informatika Telekomunikasi</td>
+            <td style="border: 2px yellowgreen solid"> D03</td>
+            <td style="border: 2px yellowgreen solid"> 15.30-18.00</td>
+        </tr>
+    </table>
+        <footer>
+            <p>&COPY;Nabilah Tanjach Muna</p>
+        </footer>
+        
+        
+             
+             
+    </body>
+</html>
